4a05d229051438210b9d5ef638d023c864ab8d3f
RyazanovAndrey <ryazanov.andrej@gmail.com>
2025-08-15 12:38:31 +0300

    new deploy

--- src/data.js
+++ src/data.js
@@ -6,15 +6,15 @@ const linksMenu = [
 ]
 
 const worksList = [
-    { image: '../versel.jpg', title: 'Versel Hotel', desc: 'Business card website Irina Boyko furniture designer', stack: ['React', 'Tailwind'], link: 'https://ryazanovandrey.github.io/hotel/'},
-    { image: '../coffelife.jpg', title: 'CoffeLife', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'CSS', 'JS'], link: 'https://ryazanovandrey.github.io/coffelife/'},
-    { image: '../consult.jpg', title: 'Consult', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'CSS', 'JS'], link: 'https://ryazanovandrey.github.io/consult/'},
-    { image: '../design_art.jpg', title: 'Design Art', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'SCSS', 'JS'], link: 'https://ryazanovandrey.github.io/design/'},
-    { image: '../fitbox.jpg', title: 'Fit Box', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'CSS', 'JS'], link: 'https://ryazanovandrey.github.io/fit/'},
-    { image: '../golden.jpg', title: 'Golden Goose', desc: 'Business card website Irina Boyko furniture designer', stack: ['React', 'SCSS'], link: 'https://golden-goose-three.vercel.app/'},
-    { image: '/mstore.jpg', title: 'M-Store', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'CSS', 'JS'], link: 'https://ryazanovandrey.github.io/mstore/'},
-    { image: '../renta.jpg', title: 'Renta Lux', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'CSS', 'JS'], link: 'https://ryazanovandrey.github.io/rentalux/'},
-    { image: '../travel_ukr.jpg', title: 'Renta Lux', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'SCSS', 'JS'], link: 'https://ryazanovandrey.github.io/travel/'}
+    { image: 'versel.jpg', title: 'Versel Hotel', desc: 'Business card website Irina Boyko furniture designer', stack: ['React', 'Tailwind'], link: 'https://ryazanovandrey.github.io/hotel/'},
+    { image: 'coffelife.jpg', title: 'CoffeLife', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'CSS', 'JS'], link: 'https://ryazanovandrey.github.io/coffelife/'},
+    { image: 'consult.jpg', title: 'Consult', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'CSS', 'JS'], link: 'https://ryazanovandrey.github.io/consult/'},
+    { image: 'design_art.jpg', title: 'Design Art', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'SCSS', 'JS'], link: 'https://ryazanovandrey.github.io/design/'},
+    { image: 'fitbox.jpg', title: 'Fit Box', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'CSS', 'JS'], link: 'https://ryazanovandrey.github.io/fit/'},
+    { image: 'golden.jpg', title: 'Golden Goose', desc: 'Business card website Irina Boyko furniture designer', stack: ['React', 'SCSS'], link: 'https://golden-goose-three.vercel.app/'},
+    { image: 'mstore.jpg', title: 'M-Store', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'CSS', 'JS'], link: 'https://ryazanovandrey.github.io/mstore/'},
+    { image: 'renta.jpg', title: 'Renta Lux', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'CSS', 'JS'], link: 'https://ryazanovandrey.github.io/rentalux/'},
+    { image: 'travel_ukr.jpg', title: 'Renta Lux', desc: 'Business card website Irina Boyko furniture designer', stack: ['HTML', 'SCSS', 'JS'], link: 'https://ryazanovandrey.github.io/travel/'}
 ]
 
 export { linksMenu, worksList }
