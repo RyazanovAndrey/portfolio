b554c7e7e45f013d0f75f63778c601e02c4673fd
RyazanovAndrey <ryazanov.andrej@gmail.com>
2025-10-02 13:44:14 +0300

    13.44

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ThemeProvider from './Context/ThemeProvider.jsx'
+import ThemeProvider from './context/ThemeProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
 
